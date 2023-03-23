4565297354f1e6641f4f3f92ed8e6ef2b8b5177c
FyMeyer <f.meyer@xsigns.de>
2023-03-23 13:23:24 +0100

    hey

--- test.java
+++ test.java
@@ -1,10 +1,11 @@
 public class test {
-       public static void main (String[] args) {
-              System.out.println("Hello World!");
-              System.out.println("Hello World! zwei");
-              if (1 == 1) {
-                     System.out.println('JaJa');
-              }
-              System.out.println('Neue Mega Nachricht');
-       }
+   public static void main (String[] args) {
+          System.out.println("Hello World!");
+          System.out.println("Hello World! zwei");
+          if (1 == 1) {
+                 System.out.println('JaJa');
+
+          }
+          System.out.println('Neue Mega Nachricht');
+   }
 }
