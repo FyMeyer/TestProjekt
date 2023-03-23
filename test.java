176596db9ea455a3648632e0534ba3819ec97db4
FyMeyer <f.meyer@xsigns.de>
2023-03-23 08:31:22 +0100

    Neues Mega Update vom Pc zur Website
    Neues Mega Update was am PC erstellt wurde

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
 public class test {
- 
        public static void main (String[] args) {
-             System.out.println("Hello World!");
-			 System.out.println("Hello World! zwei");
+              System.out.println("Hello World!");
+              System.out.println("Hello World! zwei");
+              if (test() == 1) {
+                     System.out.println('JaJa');
+              }
+              return 1
        }
 }
