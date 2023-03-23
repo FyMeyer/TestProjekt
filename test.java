b5fb973626f396e81398a391f606999047ddebad
FyMeyer <f.meyer@xsigns.de>
2023-03-23 08:36:44 +0100

    s

--- test.java
+++ test.java
@@ -2,9 +2,8 @@ public class test {
        public static void main (String[] args) {
               System.out.println("Hello World!");
               System.out.println("Hello World! zwei");
-              if (test() == 1) {
+              if (1 == 1) {
                      System.out.println('JaJa');
               }
-              return 1
        }
 }
